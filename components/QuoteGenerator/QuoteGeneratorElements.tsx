d5c4227bd1f852837a2110382b7d53fafd5f91e8
Gabriel Almeida <biel88sud@gmail.com>
2023-09-18 06:34:06 -0300

    Added footer component

--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -36,3 +36,23 @@ export const BackgroundImage2 = styled(Image)`
   right: -120px;
   bottom: -10px;
 `;
+
+export const FooterContainer = styled.div`
+  width: 100vw;
+  height: 50px;
+  text-align: center;
+  font-family: 'Source Code Pro', monospace;
+  font-size: 15px;
+  position: absolute;
+  bottom: 0;
+  color: white;
+  z-index: 999999;
+`;
+
+export const RedSpan = styled.span`
+  color: red;
+`;
+
+export const FooterLink = styled(Link)`
+  color: white;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
@@ -5,25 +6,57 @@ import {
   GradientBackgroundCon,
   BackgroundImage1,
   BackgroundImage2,
+  FooterContainer,
+  FooterLink,
+  RedSpan,
 } from "@/components/QuoteGenerator/QuoteGeneratorElements";
 
 import Clouds1 from '@/assets/cloud-and-thunder.png'
 import Clouds2 from "@/assets/cloudy-weather.png";
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null >(0);
+
   return (
     <>
       <Head>
         <title>Inspirational Quote Generator</title>
-        <meta name="description" content="A fun project to generate quotes using AWS and Next.js" />
+        <meta
+          name="description"
+          content="A fun project to generate quotes using AWS and Next.js"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Background */}
       <GradientBackgroundCon>
-        <BackgroundImage1 src={Clouds1} height="300" alt="cloudy background 1" />
-        <BackgroundImage2 src={Clouds2} height="300" alt="cloudy background 1" />
+        <BackgroundImage1
+          src={Clouds1}
+          height="300"
+          alt="cloudy background 1"
+        />
+        <BackgroundImage2
+          src={Clouds2}
+          height="300"
+          alt="cloudy background 1"
+        />
+
+        <FooterContainer>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed with <RedSpan>♥</RedSpan> by{" "}
+            <FooterLink
+              href="https://github.com/ggalmeida1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              @GabrielAlmeida{" "}
+            </FooterLink>
+          </>
+        </FooterContainer>
       </GradientBackgroundCon>
     </>
-  )
+  );
 }
